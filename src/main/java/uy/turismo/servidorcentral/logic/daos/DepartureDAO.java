083bb20f0f8942cba1b838208534c0432f49d58f
Lucas Techera <lucasfidel85@gmail.com>
2023-08-24 16:48:39 -0300

    BundleDAO y DepartureDAO
    Solo interfaces, sin implementar.

---
+++ src/main/java/uy/turismo/servidorcentral/logic/daos/DepartureDAO.java
@@ -0,0 +1,40 @@
+package uy.turismo.servidorcentral.logic.daos;
+
+import uy.turismo.servidorcentral.logic.entities.TouristicDeparture;
+
+import uy.turismo.servidorcentral.logic.entities.TouristicActivity;
+
+import java.util.List;
+
+public interface DepartureDAO {
+
+	
+	/**
+	 * 
+	 * @return Devuelve una lista con todas las salidas turisticas.
+	 */
+	public List<TouristicDeparture> findAllDepartures();
+	
+	/**
+	 * 
+	 * @param id -> de la salida turistica
+	 * @return
+	 */
+	public TouristicDeparture findDepartureById(Long id);
+	
+	/**
+	 * 
+	 * @param activityId -> identificador de la actividad turistica
+	 * @return -> Devuelve una lista de las salidas turisticas de aquellas actividades turistcas cuyo id = activityId
+	 */
+	public List<TouristicDeparture> findDepartureByActivity(TouristicActivity activityId);
+	
+	/**
+	 * 
+	 * @param departure -> salida turistica
+	 * @throws Exception -> En caso de no poder crear una salida turistica.
+	 */
+	public void createDeparture(TouristicDeparture departure) throws Exception;
+	
+	
+}
