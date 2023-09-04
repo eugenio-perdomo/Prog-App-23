6adf384d40797fbf6785150e84b55c5be82c8126
BraG29 <braianbarrios_29@hotmail.com>
2023-09-04 18:05:21 -0300

    .

--- src/main/java/uy/turismo/servidorcentral/logic/controller/MainDePruebas.java
+++ src/main/java/uy/turismo/servidorcentral/logic/controller/MainDePruebas.java
@@ -1,8 +1,11 @@
 package uy.turismo.servidorcentral.logic.controller;
 
 
+import java.util.List;
+
 import org.hibernate.Session;
 
+import uy.turismo.servidorcentral.logic.datatypes.DtUser;
 import uy.turismo.servidorcentral.logic.util.HibernateUtil;
 
 public class MainDePruebas {
@@ -10,14 +13,17 @@ public class MainDePruebas {
 	public static void main(String[] args) {
         String yellow = "\u001B[33m";
         
+        IController sys = ControllerFactory.getIController();
+//      List<DtUser> users = sys.getListUser();
+        DtUser user = sys.getUserData(1L);
+        
         System.out.println(yellow + "Info: DONE" + yellow);
         
 	}
 	
-	public void initSession() {
+	public static void initSession() {
 		Session session = HibernateUtil.getSessionFactory().openSession();
 		session.close();
-		
 	}
 
 }
