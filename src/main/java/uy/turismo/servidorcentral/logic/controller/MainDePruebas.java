826493125e51785dd77e38b9db2f20f656f6113d
ShodytheWolf <martin.antunez5@gmail.com>
2023-09-04 20:00:20 -0300

    getListDepartment(boolean) terminado MA
    lo que dice el titulo jaja xd

--- src/main/java/uy/turismo/servidorcentral/logic/controller/MainDePruebas.java
+++ src/main/java/uy/turismo/servidorcentral/logic/controller/MainDePruebas.java
@@ -1,17 +1,32 @@
 package uy.turismo.servidorcentral.logic.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.hibernate.Session;
 
+import uy.turismo.servidorcentral.logic.daos.UserDAO;
+import uy.turismo.servidorcentral.logic.daos.UserDAOImpl;
+import uy.turismo.servidorcentral.logic.datatypes.DtDepartment;
+import uy.turismo.servidorcentral.logic.datatypes.DtTourist;
 import uy.turismo.servidorcentral.logic.util.HibernateUtil;
 
 public class MainDePruebas {
 
 	public static void main(String[] args) {
 
-		String yellow = "\u001B[33m";
-//		initSession();
-		
-        System.out.println(yellow + "Info: DONE" + yellow);
+		String yellow = "\u001B[33m";      
+        Controller controlador = Controller.getInstance();
+        
+//        UserDAO user = new UserDAOImpl();
+//    	
+//    	List<DtTourist> pruebaTourist;
+//    	
+//    	pruebaTourist =  controlador.getListTourist();
+        
+        List<DtDepartment> pruebaDepartment = controlador.getListDepartment(true);
+    	
+    	System.out.println(yellow + "Info: DONE" + yellow);
         
 	}
 	
@@ -20,4 +35,5 @@ public class MainDePruebas {
 		session.close();
 	}
 
+	
 }
