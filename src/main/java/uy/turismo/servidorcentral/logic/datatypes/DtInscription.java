d13c9cded1a629a78126d77f8652074cfccfd480
eugenio-perdomo <scnhp.0894@gmail.com>
2023-09-05 19:30:45 -0300

    Update DtInscription.java

--- src/main/java/uy/turismo/servidorcentral/logic/datatypes/DtInscription.java
+++ src/main/java/uy/turismo/servidorcentral/logic/datatypes/DtInscription.java
@@ -2,26 +2,58 @@ package uy.turismo.servidorcentral.logic.datatypes;
 
 import java.time.LocalDate;
 
-public class DtInscription {
+import uy.turismo.servidorcentral.logic.entities.Tourist;
+import uy.turismo.servidorcentral.logic.entities.TouristicDeparture;
+
+public class DtInscription extends DtBaseEntity {
 
 	private LocalDate inscriptionDate;
 	private Double totalCost;
 	private Integer touristAmount;
-	private Long touristId;
-	private Long departureId;
+	private Tourist tourist;
+	private TouristicDeparture departure;
 	
-	public DtInscription(LocalDate inscriptionDate, Double totalCost, Integer touristAmount, Long touristId,
-			Long departureId) {
-		super();
+	public DtInscription() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public DtInscription(Long id) {
+		super(id);
+		// TODO Auto-generated constructor stub
+	}	
+	
+	public DtInscription(Long id,
+			LocalDate inscriptionDate,
+			Double totalCost,
+			Integer touristAmount,
+			Tourist tourist,
+			TouristicDeparture departure) {
+		super(id);
 		this.inscriptionDate = inscriptionDate;
 		this.totalCost = totalCost;
 		this.touristAmount = touristAmount;
-		this.touristId = touristId;
-		this.departureId = departureId;
+		this.tourist = tourist;
+		this.departure = departure;
+	}
+		
+	public LocalDate getInscriptionDate() {
+		return inscriptionDate;
 	}
 	
+	public Double getTotalCost() {
+		return totalCost;
+	}
 	
+	public Integer getTouristAmount() {
+		return touristAmount;
+	}
 	
+	public Tourist getTourist() {
+		return tourist;
+	}
 	
-	
+	public TouristicDeparture getTouristicDeparture() {
+		return departure;
+	}
+
 }
