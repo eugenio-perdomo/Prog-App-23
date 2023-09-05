74202541b9813c40195c415e69433a2a57fe9d2f
BraG29 <braianbarrios_29@hotmail.com>
2023-09-05 13:36:18 -0300

    no push

--- src/main/java/uy/turismo/servidorcentral/logic/controller/MainDePruebas.java
+++ src/main/java/uy/turismo/servidorcentral/logic/controller/MainDePruebas.java
@@ -2,6 +2,8 @@ package uy.turismo.servidorcentral.logic.controller;
 
 import org.hibernate.Session;
 
+import uy.turismo.servidorcentral.logic.datatypes.DtTourist;
+import uy.turismo.servidorcentral.logic.datatypes.DtUser;
 import uy.turismo.servidorcentral.logic.util.HibernateUtil;
 
 public class MainDePruebas {
@@ -9,6 +11,24 @@ public class MainDePruebas {
 	public static void main(String[] args) {
 
 		String yellow = "\u001B[33m";
+		
+		IController sys = ControllerFactory.getIController();
+		
+		DtTourist userData = (DtTourist) sys.getUserData(5L);
+		
+		DtUser updatedData = new DtTourist(
+				userData.getId(),
+				userData.getName(),
+				userData.getNickname(),
+				userData.getEmail(),
+				userData.getLastName(),
+				userData.getBirthDate(),
+				"canadiense",
+				null
+				);
+		
+		sys.updateUser(updatedData);
+		
 		
 //		initSession();
 		
